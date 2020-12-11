5320987a09e7ff5728bf86cfa681100521535bf8
wireboy5 <30930649+wireboy5@users.noreply.github.com>
2020-12-11 18:17:54 -0500

    Working on 64 bit bootstrap
    Signed-off-by: wireboy5 <30930649+wireboy5@users.noreply.github.com>

--- kernel/kernel.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <kernel/x86/drivers/screen.h>
-#include <kernel/x86/drivers/timer.h>
-#include <kernel/x86/dt/gdt.h>
-#include <kernel/x86/dt/idt.h>
-
-
--- kernel/x86/libc/math.cpp
+++
@@ -1,6 +0,0 @@
-#include "math.h"
-
-int abs(int n){
-    return n > 0 ? n : n*-1;
-}
-
