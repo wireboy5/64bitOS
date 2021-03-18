229af88e7c7d1ab3aa681437420a3b9f2bd10e59
wireboy5 <30930649+wireboy5@users.noreply.github.com>
2021-03-18 01:05:26 -0400

    And that is long mode!
    Signed-off-by: wireboy5 <30930649+wireboy5@users.noreply.github.com>

---
+++ kernel/asm/mboot.asm
@@ -0,0 +1,16 @@
+section .multiboot_header
+header_start:
+    ; Multiboot 2 magic number
+    dd 0xe85250d6
+    dd 0
+    ; Header length
+    dd header_end - header_start
+
+    ; Checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; End tag
+    dw 0
+    dw 0
+    dd 8
+header_end:
