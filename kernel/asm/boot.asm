5d23323b72e895b09d38116c53568110b2a443dd
Riley Wilton <meetingprince34@outlook.com>
2021-03-19 15:04:06 -0400

    What is going on???

--- kernel/asm/boot.asm
+++ kernel/asm/boot.asm
@@ -19,7 +19,7 @@ _start:
 
     ; Initialize paging
     call init_paging
-
+    jmp $
     ; Load GDT
     lgdt [gdt64.pointer-offset]
 
@@ -29,41 +29,37 @@ _start:
 
 ; Setup Page tables
 init_paging:
-    mov eax, pdpt - offset
+    ; Map first PML4 Entry
+    mov eax, pdpt
     or eax, 0b11
     mov [pml4 - offset], eax
 
-    mov eax, pd - offset
+    ; Map the first PDPT entry
+    mov eax, pd
     or eax, 0b11
     mov [pdpt - offset], eax
 
-    mov ecx, 0
-.map_pd
+.map_table
+    ; We want eax to contain the size of the page
+    ; (2MB) * the index in the page table.
     mov eax, 0x200000
     mul ecx
+    ; Now we will set the
+    ; present, writable, and huge bits.
     or eax, 0b10000011
-    mov [pd - offset + ecx * 8], eax
 
+    ; And move it into the page directory
+    mov [pd - offset + ecx * 8 + 3 * 8], eax
 
+    ; Increment the counter
     inc ecx
+
+    ; And if it is 512, we have mapped the entire table
     cmp ecx, 512
-    jne .map_pd
+    jne .map_table
 
-    mov eax, pml4 - offset
-    mov cr3, eax
+    ; At this point, we have mapped all tables.
 
-    mov eax, cr4
-    or eax, 1 << 5
-    mov cr4, eax
-
-    mov ecx, 0xC0000080
-    rdmsr
-    or eax, 1 << 8
-    wrmsr
-
-    mov eax, cr0
-    or eax, 1 << 31
-    mov cr0, eax
 
     ret
 
