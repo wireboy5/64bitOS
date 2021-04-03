ea35b052f2c4674d55452f6ff8b975dbe3b56060
wireboy5 <30930649+wireboy5@users.noreply.github.com>
2021-04-03 19:30:08 -0400

    Some tests, update gitignore
    Signed-off-by: wireboy5 <30930649+wireboy5@users.noreply.github.com>

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -5,7 +5,13 @@ extern "C" size_t kernelOffset;
 extern "C" size_t _kernel_start;
 
 extern "C" int kmain () {
-    char* vidmem = (char*)0xb8000;
-    vidmem[0] = 'C';
+    // Test that lower 2MB works
+    char* vidmem = (char*)(0xb8000);
+    vidmem[0] = 'L';
+
+    // Test that higher half works
+    char* vidmem_high = (char*)(0xb8000 + 0xC0000000);
+    vidmem[2] = 'H';
+
     return 0;
 }
