58a3b388dbc88fb802441c469400a1f7dbaf999e
wireboy5 <30930649+wireboy5@users.noreply.github.com>
2021-04-03 18:58:35 -0400

    A Bit of cleanup
    Signed-off-by: wireboy5 <30930649+wireboy5@users.noreply.github.com>

--- kernel/asm/entry.asm
+++ kernel/asm/entry.asm
@@ -3,13 +3,14 @@ extern kmain
 section .text
 bits 64
 entry:
-    ; load 0 into all data segment registers
-    mov ax, 0
+    ; Reset the data segment registers
+    mov ax, 0x0
     mov ss, ax
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
+
     ; Call the kernel
     call kmain
     hlt
