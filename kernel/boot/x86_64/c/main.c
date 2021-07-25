73bc898fbcf4ef90f28fa6404db1dbe2c676ec3f
wireboy5 <30930649+wireboy5@users.noreply.github.com>
2021-07-25 13:57:39 -0400

    Ok???
    Signed-off-by: wireboy5 <30930649+wireboy5@users.noreply.github.com>

--- kernel/boot/x86_64/c/main.c
+++ kernel/boot/x86_64/c/main.c
@@ -13,8 +13,8 @@ extern void kmain(void* multiboot_info) {
     sysinfo_t info = get_sysinfo(multiboot_info);
 
     // Test frame buffer
-    for(int y = 0; y < info.fb.height; y++) {
-        for(int x = 0; x < info.fb.width; x++) {
+    for(uint32_t y = 0; y < info.fb.height; y++) {
+        for(uint32_t x = 0; x < info.fb.width; x++) {
             set_pixel(info, x, y, 0xFFFFFFFF);
         }
     }
