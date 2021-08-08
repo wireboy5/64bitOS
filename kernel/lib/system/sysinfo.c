973cb5426d09114cfa43638878c5158bf3c202e9
Riley Wilton <riley.j.wilton@gmail.com>
2021-08-08 18:44:52 -0400

    Stuff.

--- kernel/lib/system/sysinfo.c
+++ kernel/lib/system/sysinfo.c
@@ -1,29 +1,8 @@
 #include "sysinfo.h"
 
 
-void parse_framebuffer(sysinfo_t* info, struct multiboot_tag_framebuffer* fb) {
-    info->fb.address = fb->common.framebuffer_addr;
-    info->fb.pitch = fb->common.framebuffer_pitch;
-    info->fb.width = fb->common.framebuffer_width;
-    info->fb.height = fb->common.framebuffer_height;
-    info->fb.bpp = fb->common.framebuffer_bpp;
-    info->fb.type = fb->common.framebuffer_type;
-    if(info->fb.type == 0) {
-        info->fb.color.indexed.framebuffer_palette_num_colors = fb->framebuffer_palette_num_colors;
-        info->fb.color.indexed.framebuffer_palette = &(fb->framebuffer_palette[0]);
-        serial_print(" - Indexed");
-    } else if (info->fb.type == 1) {
-        info->fb.color.rgb.red_field_position = fb->framebuffer_red_field_position;
-        info->fb.color.rgb.green_field_position = fb->framebuffer_green_field_position;
-        info->fb.color.rgb.blue_field_position = fb->framebuffer_blue_field_position;
-        info->fb.color.rgb.red_mask_size = fb->framebuffer_red_mask_size;
-        info->fb.color.rgb.green_mask_size = fb->framebuffer_green_mask_size;
-        info->fb.color.rgb.blue_mask_size = fb->framebuffer_blue_mask_size;
-        serial_print(" - RGB");
-    }
-}
 
-void parse_multiboot_info(void* multiboot_info, sysinfo_t* info) {
+void sysinfo_populate_multiboot_info(void* multiboot_info, sysinfo_t* info) {
 
     // Grab tags header
     info->tags_header = *(multiboot_tags_header_t*)multiboot_info;
@@ -48,66 +27,165 @@ void parse_multiboot_info(void* multiboot_info, sysinfo_t* info) {
         switch (tag->type) {
             case MULTIBOOT_TAG_TYPE_CMDLINE:
                 tag_type = "Command Line";
+
+                // Increment num_cmdlines
+                info->num_cmdlines++;
+
                 break;
             case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
                 tag_type = "Boot Loader Name";
+                
+                // Cast tag
+                struct multiboot_tag_string* tag_string = (struct multiboot_tag_string*)tag;
+
+                // Set the boot loader name
+                info->bootloader_name = tag_string->string;
+
                 break;
             case MULTIBOOT_TAG_TYPE_MODULE:
                 tag_type = "Module";
+
+                // Increment num_modules
+                info->num_modules++;
+
                 break;
             case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
                 tag_type = "Basic Memory Information";
+                // Ignore this tag
                 break;
             case MULTIBOOT_TAG_TYPE_BOOTDEV:
                 tag_type = "Boot Device";
+                
+                // Cast the tag
+                struct multiboot_tag_bootdev* bootdev = (struct multiboot_tag_bootdev*)tag;
+
+                // Set boot device info
+                info->bootdev_info.biosdev = bootdev->biosdev;
+                info->bootdev_info.partition = bootdev->slice;
+                info->bootdev_info.sub_part = bootdev->part;
+
                 break;
             case MULTIBOOT_TAG_TYPE_MMAP:
                 tag_type = "Memory Map";
+
+                // Set the address of the memory map
+                info->mboot_mmap_addr = tag;
+                
                 break;
             case MULTIBOOT_TAG_TYPE_VBE:
                 tag_type = "VBE";
+                // Ignore for now
                 break;
             case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
-                tag_type = "Framebuffer";
+                // Cast tag
+                struct multiboot_tag_framebuffer* fb = (struct multiboot_tag_framebuffer*)tag;
+
+                // Set framebuffer info
+                info->fb.address = fb->common.framebuffer_addr;
+                info->fb.pitch = fb->common.framebuffer_pitch;
+                info->fb.width = fb->common.framebuffer_width;
+                info->fb.height = fb->common.framebuffer_height;
+                info->fb.bpp = fb->common.framebuffer_bpp;
+                info->fb.type = fb->common.framebuffer_type;
+
+                if(info->fb.type == 0) {
+                    info->fb.color.indexed.framebuffer_palette_num_colors = fb->framebuffer_palette_num_colors;
+                    info->fb.color.indexed.framebuffer_palette = &(fb->framebuffer_palette[0]);
+                    tag_type = "Framebuffer (Indexed)";
+                } else if (info->fb.type == 1) {
+                    info->fb.color.rgb.red_field_position = fb->framebuffer_red_field_position;
+                    info->fb.color.rgb.green_field_position = fb->framebuffer_green_field_position;
+                    info->fb.color.rgb.blue_field_position = fb->framebuffer_blue_field_position;
+                    info->fb.color.rgb.red_mask_size = fb->framebuffer_red_mask_size;
+                    info->fb.color.rgb.green_mask_size = fb->framebuffer_green_mask_size;
+                    tag_type = "Framebuffer (RGB)";
+                } else {
+                    tag_type = "Framebuffer (EGA)";
+                }
+                
+
+                // Set framebuffer info
+                parse_framebuffer(info, (struct multiboot_tag_framebuffer*)tag);
+                
                 break;
             case MULTIBOOT_TAG_TYPE_ELF_SECTIONS:
                 tag_type = "ELF Sections";
+                // Ignore for now
                 break;
             case MULTIBOOT_TAG_TYPE_APM:
                 tag_type = "APM";
+                // Ignore for now
                 break;
             case MULTIBOOT_TAG_TYPE_EFI32:
                 tag_type = "EFI32";
+                // Ignore this
                 break;
             case MULTIBOOT_TAG_TYPE_EFI64:
                 tag_type = "EFI64";
+
+                // Cast tag
+                struct multiboot_tag_efi64* efi64 = (struct multiboot_tag_efi64*)tag;
+
+                // Set address of efi tables
+                info->efi64 = efi64->pointer;
+
                 break;
             case MULTIBOOT_TAG_TYPE_SMBIOS:
                 tag_type = "SMBIOS";
+                // Ignore for now
                 break;
             case MULTIBOOT_TAG_TYPE_ACPI_OLD:
-                tag_type = "ACPI RDSPv1";
+                tag_type = "ACPI RSDPv1";
+
+                // Set pointer to this tag + size of basic tag header
+                info->RSDPv1 = (uint32_t)tag + 8;
+
                 break;
             case MULTIBOOT_TAG_TYPE_ACPI_NEW:
                 tag_type = "ACPI RSDPv2";
+
+                // Same as ACPI OLD
+                info->RSDPv2 = (uint32_t)tag + 8;
+
                 break;
             case MULTIBOOT_TAG_TYPE_NETWORK:
                 tag_type = "Network";
+                // Ignore for now
                 break;
             case MULTIBOOT_TAG_TYPE_EFI_MMAP:
                 tag_type = "EFI Memory Map";
+
+                // Set the address of the EFI memory map
+                info->efi_mmap = tag + 8;
+
                 break;
             case MULTIBOOT_TAG_TYPE_EFI_BS:
                 tag_type = "EFI Boot Services";
+                // Ignore for now
                 break;
             case MULTIBOOT_TAG_TYPE_EFI32_IH:
                 tag_type = "EFI32 IH";
+                // Ignore this
                 break;
             case MULTIBOOT_TAG_TYPE_EFI64_IH:
                 tag_type = "EFI64 IH";
+
+                // Cast tag
+                struct multiboot_tag_efi64_ih* efi64_ih = (struct multiboot_tag_efi64_ih*)tag;
+
+                // Set address of efi Image Handle
+                info->efi64_ih = efi64_ih->pointer;
+
                 break;
             case MULTIBOOT_TAG_TYPE_LOAD_BASE_ADDR:
                 tag_type = "Load Base Address";
+
+                // Cast tag
+                struct multiboot_tag_load_base_addr* load_base_addr = (struct multiboot_tag_load_base_addr*)tag;
+
+                // Set load base address
+                info->load_base = load_base_addr->load_base_addr;
+
                 break;
         } 
 
@@ -116,16 +194,20 @@ void parse_multiboot_info(void* multiboot_info, sysinfo_t* info) {
     }
 }
 
+void populate_derived(sysinfo_t* sysinfo) {
+
+}
+
 sysinfo_t get_sysinfo(void* multiboot_info) {
     log(1, "Retrieving system information.\n");
     sysinfo_t sysinfo;
 
     // Parse multiboot info
-    parse_multiboot_info(multiboot_info, &sysinfo);
+    sysinfo_populate_multiboot_info(multiboot_info, &sysinfo);
 
-
+    // Populate the rest of the system info based off of what we have found.
+    populate_derived(&sysinfo);
     
-
     /*
     // Generate info for page frame alocator
 
--- kernel/lib/system/sysinfo.h
+++ kernel/lib/system/sysinfo.h
@@ -8,64 +8,99 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-
-struct page_allocator_info {
-    // The last address that needs to be paged in.
-    uint64_t page_to;
-
-    // The number of pages to that address
-    uint64_t num_pages;
-
-    // The calculated bitmap size
-    uint64_t bitmap_size;
-
-    // The bitmap address
-    uint64_t bitmap_addr;
-};
-typedef struct page_allocator_info page_allocator_info_t;
-
-
-struct memmap_entry {
-    uint64_t start;
-    uint64_t size;
-    uint8_t flags;
-};
-typedef struct memmap_entry memmap_entry_t;
-
-struct memmap {
-    memmap_entry_t entries[4096];
-    uint32_t index;
-};
-typedef struct memmap memory_map_t;
-
 struct multiboot_tags_header {
     uint32_t total_size;
     uint32_t reserved;
 };
 typedef struct multiboot_tags_header multiboot_tags_header_t;
 
+struct boot_device_info {
+    uint32_t biosdev; // Bios disk device
+    uint32_t partition; // The top level partition
+    uint32_t sub_part; // The sub partition.
+};
+typedef struct boot_device_info boot_device_info_t;
 
+struct framebuffer {
+    uint64_t address;
+    uint64_t pitch;
+    uint32_t width;
+    uint32_t height;
+    uint8_t  bpp;
+    uint8_t  type;
+    uint16_t reserved;
+
+    union
+    {
+        struct
+        {
+            uint16_t framebuffer_palette_num_colors;
+            struct multiboot_color* framebuffer_palette;
+        } indexed;
+        struct
+        {
+            uint8_t red_field_position;
+            uint8_t red_mask_size;
+            uint8_t green_field_position;
+            uint8_t green_mask_size;
+            uint8_t blue_field_position;
+            uint8_t blue_mask_size;
+        } rgb;
+    } color;
+};
+
+
+typedef struct framebuffer framebuffer_t;
 
 struct sysinfo {
-    framebuffer_t fb;
+    // These are initialized along side the multiboot header
+    // If any of these values are NULL then multiboot did not provide them
+    // and the computer may not support them. (Eg. RDSPv2 on ACPI 1.0 systems.)
+
+    // The multiboot tag header
     multiboot_tags_header_t tags_header;
-    struct multiboot_tag_mmap* grub_memmap;
-    memory_map_t* memmap;
 
-    uint64_t mem_lower;
-    uint64_t mem_upper;
+    // The number of command lines provided by bootloader.
+    uint64_t num_cmdlines;
 
-    char* cmdline;
-    char* boot_loader_name;
+    // The name of the bootloader.
+    char* bootloader_name;
 
+    // The number of modules discovered.
+    uint64_t num_modules;
+
+    // The boot device information.
+    boot_device_info_t bootdev_info;
+
+    // The address of the memory map provided by multiboot
+    uint64_t mboot_mmap_addr;
+
+    // Pointer to the framebuffer
+    framebuffer_t fb;
+
+    // The address of the 64 bit EFI tables
     uint64_t efi64;
 
-    struct multiboot_tag_old_acpi* RSDPv1;
-    struct multiboot_tag_new_acpi* RSDPv2;
+    // The address of the RSDPv1 table
+    uint64_t RSDPv1;
 
-    struct multiboot_tag_efi_mmap* efi_memmap;
+    // The address of the RSDPv2 table
+    uint64_t RSDPv2;
 
-    page_allocator_info_t page_allocator_info;
+    // The address of the EFI memory map
+    uint64_t efi_mmap;
+
+    // The 64 bit EFI image handle
+    uint64_t efi64_ih;
+
+    // The load base address
+    uint64_t load_base;
+
+    
+    // These will be initialized after the previous values are initialized
+    // None of these should ever be NULL.
+
+
 };
 typedef struct sysinfo sysinfo_t;
 
@@ -73,3 +108,12 @@ typedef struct sysinfo sysinfo_t;
 // and various other sources.
 sysinfo_t get_sysinfo(void* multiboot_info);
 
+/*
+    Populates the system info using the multiboot header.
+*/
+void sysinfo_populate_multiboot_info(void* multiboot_info, sysinfo_t* info);
+
+/*
+    Populates sysinfo further using already discovered information
+*/
+void sysinfo_populate_derived(sysinfo_t* sysinfo);
